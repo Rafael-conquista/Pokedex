67e9f6e1ee90d9c599bca9634d5babbd5e86c65d
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2024-01-05 21:48:50 -0300

    inicia desenvolvimento de sugestão de pesquisa

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,27 @@
 import React, { useState, useContext } from "react";
-import FavoriteContext from "../contexts/favoritesContext";
+import pokemon_names from "./poke_names";
 
 const Searchbar = (props) => {
     const [search, setSearch] = useState("dito")
+    const [ list, setList ] = useState([])
     const { onSearch } = props
 
+    const compareSearch = (string) =>{
+        if (string.length > 1){
+           let similar_list = pokemon_names.filter(elemento => elemento.toLowerCase().startsWith(string))
+           setList(similar_list)
+           console.log(list)
+        }
+    }
+
     const onChangeHandler = (e) => {
-        setSearch(e.target.value)
+        let string = e.target.value
+        string = string.toLowerCase()
+        setSearch(string)
+        compareSearch(string)
         if (e.target.value.length === 0) {
             onSearch(undefined)
+            setList([])
         }
     }
 
@@ -23,6 +36,15 @@ const Searchbar = (props) => {
         <div className="searchbar-container">
             <div className="searchbar">
                 <input placeholder="Buscar pokemon" onChange={onChangeHandler} id="search" />
+                <div>
+                    {list.map((name) => {
+                        return (
+                            <div className=''>
+                                <span>{name}</span>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             <div className="searchbar-btn">
                 <button onClick={onButtonClickHandler} >Buscar</button>
